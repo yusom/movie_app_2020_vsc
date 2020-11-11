583e498009422e9073b5f34db0e2d095bf29d934
yusom <96_chi@naver.com>
2020-11-11 15:15:46 +0900

    Movie_style

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Movie App 2020 vsc
 
-React JS Fundamentals Course (2020 Update!)
+React JS Fundamentals Course
--- src/Movie.js
+++ src/Movie.js
@@ -14,7 +14,7 @@ function Movie({ year,title,summary,poster,genres}){
                         <li key = {index} li className='genres_genre'>{genre}</li>
                     ))}
                 </ul>
-                <p className='movie_summary'>{summary}</p>
+                <p className='movie_summary'>{summary.slice(0, 140)}...</p>
             </div>
         </div>
     );
